d55df7bf22e5ec38ee7447364bf88987d3fe73aa
Trang Doan <doantrangltt@gmail.com>
2026-04-12 17:05:34 +0700

    feat: connect mongoDB Atlas

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -6,10 +7,15 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var dropdragRouter = require('./routes/dropdrag');
+
+const connectDB = require('./config/db');
+
+connectDB();
 
 var app = express();
 
-// view engine setup
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
@@ -21,7 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/dropdrag',dropdragRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
