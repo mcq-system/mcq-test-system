ace6d6ead703dc27ce013c84d6ba0eee71d1a2e1
Angel Git <228355042+khanh779-9@users.noreply.github.com>
2026-04-20 14:56:57 +0700

    update db

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
  */
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect('mongodb://127.0.0.1:27017/mcq-test-system-tram-be');
+    const conn = await mongoose.connect(process.env.MONGODB_URL);
     console.log(`Connect to MongoDB: ${conn.connection.host}`);
     console.log(`Sử dụng database: ${conn.connection.name}`);
   } catch (error) {
